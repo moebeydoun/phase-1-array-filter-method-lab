05f603f5ff11386dd96b1447e1829c3688e71d1f
moe beydoun <moebeydoun24@yahoo.com>
2023-11-15 15:19:50 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(drivers, name) {
+   
+    const matchingDrivers = drivers.filter(driver =>
+      driver.toLowerCase() === name.toLowerCase()
+    );
+  
+    return matchingDrivers;
+  }
+  function fuzzyMatch(drivers, query) {
+    
+    const matchingDrivers = drivers.filter(driver =>
+      driver.toLowerCase().startsWith(query.toLowerCase())
+    );
+  
+    return matchingDrivers;
+  }
+  function matchName(drivers, name) {
+   
+    const matchingDrivers = drivers.filter(driver =>
+      driver.name.toLowerCase() === name.toLowerCase()
+    );
+  
+    return matchingDrivers;
+  }
